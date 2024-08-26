51d942cd17297dfacd2eb198eb134aebdf63d226
Opilane TTHK <opilane@tthk.ee>
2024-08-26 16:40:04 +0300

    Added models for student, course, and enrollment

---
+++ Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        [Key] // Primary Key
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
