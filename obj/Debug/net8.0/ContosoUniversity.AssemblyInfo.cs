17277bcb52166b5f0cc085484f9bc12869008ece
Henry <opilane@tthk.ee>
2024-09-12 11:45:54 +0300

    Started working on create for instructors

--- obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c58d078d86653c43ba84460c64187eff4ebcf02b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ab385a92681a95e438633858faba5d2bc915051")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
